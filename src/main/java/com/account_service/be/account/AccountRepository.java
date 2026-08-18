3a7f776fae8add279181dcff7973e330c878d7d1
Moh Eka Syafrino Nazhifan <bloodsuker18@gmail.com>
2026-08-18 23:36:30 +0700

    feat: add custom query to retrieve NamesResponseDto by user IDs

--- src/main/java/com/account_service/be/account/AccountRepository.java
+++ src/main/java/com/account_service/be/account/AccountRepository.java
@@ -3,6 +3,8 @@ package com.account_service.be.account;
 import com.account_service.be.account.dto.NamesResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,6 +19,7 @@ public interface AccountRepository extends JpaRepository<AccountModel, Integer>,
     // Ambil projection/DTO fleksibel
     <T> T findByUserId(Integer userId, Class<T> type);
 
-    List<NamesResponseDto> findByUserIdIn(Integer[] userIds);
+    @Query("SELECT new com.account_service.be.account.dto.NamesResponseDto(a.userId, a.fullName, a.email) FROM AccountModel a WHERE a.userId IN :userIds")
+    List<NamesResponseDto> findByUserIdIn(@Param("userIds") Integer[] userIds);
 
 }
--- src/main/java/com/account_service/be/account/dto/NamesResponseDto.java
+++ src/main/java/com/account_service/be/account/dto/NamesResponseDto.java
@@ -1,11 +1,16 @@
 package com.account_service.be.account.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NamesResponseDto {
     private Integer userId;
     private String fullName;
+    private String email;
 }
